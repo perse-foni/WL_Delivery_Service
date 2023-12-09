6c041aade5384dbc65b5b03219bf4ff85da8165c
evatsiavi <evatsiavi@gmail.com>
2023-12-09 15:54:38 +0200

    #4 add registration page, login, registration form components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RegistrationPageComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 
 
 export const routes: Routes = [
 
     { path:'login', component:LandingPageComponent},
     { path:'home', component:HomePageComponent},
+    { path:'registration', component:RegistrationPageComponent},
 
     { path: '', redirectTo: 'login', pathMatch: 'full'},  
     { path: '**', component:PageNotFoundComponent}
