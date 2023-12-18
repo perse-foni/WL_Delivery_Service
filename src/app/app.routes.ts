3111b3058fe731149613bbbede3259f84f114a2c
kogker <kgkerzeli@athtech.gr>
2023-12-18 13:36:49 +0200

    #15 Add Order History page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HistoryPageComponent } from './components/history-page/history-page.component';
 
 
 export const routes: Routes = [
 
     { path:'login', component:LandingPageComponent},
     { path:'home', component:HomePageComponent},
+    { path: 'history', component:HistoryPageComponent},
 
     { path: '', redirectTo: 'login', pathMatch: 'full'},  
     { path: '**', component:PageNotFoundComponent}
