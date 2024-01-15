7049cdbacd335a501f6ae1855ba2721eea6a03fa
evatsiavi <evatsiavi@gmail.com>
2024-01-15 20:15:02 +0200

    create most famous stores and add json data

---
+++ src/app/components/most-famous-stores-list/most-famous-stores-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MostFamousStoreComponent } from '../most-famous-store/most-famous-store.component';
+import { FamousStoresService } from '../../services/famous-stores.service';
+import { MostFamous } from '../../most-famous';
+
+@Component({
+  selector: 'app-most-famous-stores-list',
+  standalone: true,
+  imports: [CommonModule, MostFamousStoreComponent],
+  templateUrl: './most-famous-stores-list.component.html',
+  styleUrl: './most-famous-stores-list.component.css'
+})
+export class MostFamousStoresListComponent {
+  
+  mostFamousStores: any;
+  service = inject(FamousStoresService);
+  mostFamousStore: MostFamous[] = [];
+  
+
+  ngOnInit() {
+    this.service.getMostFamous().subscribe({
+      next: data => {
+        console.log(data);
+        this.mostFamousStores = data;
+      },
+    });
+  }
+}
