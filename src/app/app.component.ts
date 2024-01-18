7852bd36860eee8b333fd131d254250579a8e72e
persefoni <ppaouri@gmail.com>
2024-01-18 20:38:16 +0200

    scroll to top component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgxScrollTopModule } from 'ngx-scrolltop';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { LandingPageComponent } from './components/landing-page/sign-in-form/landing-page.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
@@ -9,9 +10,17 @@ import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationBarComponent, FooterComponent, RegistrationPageComponent, LandingPageComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NgxScrollTopModule,
+    NavigationBarComponent,
+    FooterComponent,
+    RegistrationPageComponent,
+    LandingPageComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'WL_Delivery_Service';
