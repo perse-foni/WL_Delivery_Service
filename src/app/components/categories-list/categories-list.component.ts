9054254ada5ac2905b01680bf2a718644ad11c49
persefoni <ppaouri@gmail.com>
2024-01-27 18:42:38 +0200

    carousel component functionality and styling for categories-list component

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -3,28 +3,33 @@ import { CommonModule } from '@angular/common';
 import { CategoryService } from '../../services/category.service';
 import { CategoryComponent } from '../category/category.component';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-categories-list',
   standalone: true,
-  imports: [CommonModule, CategoryComponent, NgxSkeletonLoaderModule],
+  imports: [
+    CommonModule,
+    CategoryComponent,
+    NgxSkeletonLoaderModule,
+    CarouselModule,
+  ],
   templateUrl: './categories-list.component.html',
-  styleUrl: './categories-list.component.css'
+  styleUrl: './categories-list.component.css',
 })
 export class CategoriesListComponent {
- 
   service = inject(CategoryService);
   categoriesList: any = [];
   skeletons: number[] = Array(5);
 
   ngOnInit() {
     this.service.getCategory().subscribe({
-      next: data => {
+      next: (data) => {
         setTimeout(() => {
           console.log(data);
           this.categoriesList = data;
         }, 1000);
-      }
-    })
-  }}
+      },
+    });
+  }
+}
