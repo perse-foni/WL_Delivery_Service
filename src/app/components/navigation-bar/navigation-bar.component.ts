dbd0035dbb3ba02712e6e58b5da53da2dc408055
kogker <kgkerzeli@athtech.gr>
2024-01-29 21:27:47 +0200

    searchbar search store by name #26

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,24 +1,45 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CartComponent } from '../cart/cart.component';
 import { CartService } from '../../services/cart.service';
+import {FormControl, FormGroup, ReactiveFormsModule, FormBuilder} from '@angular/forms';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink, CartComponent],
+  imports: [CommonModule, RouterLink, CartComponent, ReactiveFormsModule],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrl: './navigation-bar.component.css',
 })
 export class NavigationBarComponent {
+  service = inject(CartService);
+  serviceStores = inject(StoresService);
+  @Input() searchBarText: any;
+  searchForm!: FormGroup;
 
-service = inject (CartService)
+  getTotalQuantity() {
+    return Math.max(
+      this.service.getItems().reduce((acc, item) => acc + item.quantity, 0),
+      0
+    );
+  }
 
-getTotalQuantity() {
-  return Math.max(
-    this.service.getItems().reduce((acc, item) => acc + item.quantity, 0),
-    0
-  );
-}
+  constructor(private formBuilder: FormBuilder) {
+    this.searchForm = new FormGroup({
+      searchValue: new FormControl(''),
+    });
+  }
+
+  onSearch() {
+    console.log('Search Form Values:', this.searchForm.value.searchValue);
+    this.serviceStores.getStores().subscribe((stores) => {
+      let searchValue = this.searchForm.value.searchValue;
+      let foundStores = stores.filter((store) =>
+        store.name.toLowerCase().includes(searchValue)
+      );
+      console.log(foundStores);
+    });
+  }
 }
