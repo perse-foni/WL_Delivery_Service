a8b1f8dca34cf3c58a3ee78881b46e0ca1e7687b
evatsiavi <evatsiavi@gmail.com>
2024-01-30 20:20:05 +0200

    fix no-stores routing

--- src/app/components/no-stores/no-stores.component.ts
+++ src/app/components/no-stores/no-stores.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-no-stores',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './no-stores.component.html',
   styleUrl: './no-stores.component.css'
 })
